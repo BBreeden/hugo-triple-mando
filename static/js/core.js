ecd86217e4478dd2d27b360c0c68c2819a5c08a2
Brian Breeden <breedenb@vt.edu>
2021-12-08 21:22:27 -0500

    added full functionality to the image hide and show

--- static/js/core.js
+++ static/js/core.js
@@ -1,9 +1,9 @@
 function hide_show_img(button_element) {
     var img = document.getElementById(button_element.value);
-    if (img.classList.contains('hide')) {
-        img.classList.remove('hide');
+    if (img.classList.contains('show')) {
+        img.classList.remove('show');
       } else {
-        img.classList.add('hide');
+        img.classList.add('show');
       }
     // alert(button_element.innerHTML);
     // TODO I need to update the inner html to change the + to a -
